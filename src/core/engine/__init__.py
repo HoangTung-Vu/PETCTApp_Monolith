a64727dfcdb9ee83948909df6858ab1c1a80700b
HoangTung-Vu <minhhoangtungvu04@gmail.com>
2026-03-03 09:08:58 +0700

    comment out old architecture

--- src/core/engine/__init__.py
+++ src/core/engine/__init__.py
@@ -1,7 +1,8 @@
-from .base import SegmentationEngine
-from .totalseg_engine import TotalSegEngine
-from .nnunet_engine import NNUNetEngine
-from .autopet_interactive_engine import AutoPETInteractiveEngine
+# from .base import SegmentationEngine
+# from .totalseg_engine import TotalSegEngine
+# from .nnunet_engine import NNUNetEngine
+# from .autopet_interactive_engine import AutoPETInteractiveEngine
 from .report_engine import ReportEngine
 
-__all__ = ["SegmentationEngine", "NNUNetEngine", "TotalSegEngine", "AutoPETInteractiveEngine", "ReportEngine"]
+# __all__ = ["SegmentationEngine", "NNUNetEngine", "TotalSegEngine", "AutoPETInteractiveEngine", "ReportEngine"]
+__all__ = ["ReportEngine"]
